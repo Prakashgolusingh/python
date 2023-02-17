cf4d0f7c05f242a6c3597ba39335d2cc2d08783e
Prakashgolusingh <95623466+Prakashgolusingh@users.noreply.github.com>
2023-02-17 19:54:16 +0530

    Update hello_world.py

--- Hello World/hello_world.py
+++ Hello World/hello_world.py
@@ -1,5 +1,19 @@
-print('Hello, World!')
-print(1 + 2)
-print(7 * 6)
-print()
-print("The end", "or is it?", "keep watching to learn more about Python", 3)
+print("hello")              # print string using double quote
+print('hello')              # print string using single quote
+print()                     # it will print a blank line
+print(6)                    # it will print number
+print('hell', 6)            # it will print string with no. 6 concate with a space between them because of comma
+print("hello", "world",)     # no error with extra comma only warning
+it = 5                        # declration of data type not necessary in python
+strs = "first string"
+print(it, strs)               # we cannot add int and string in python.
+# i.e.,data in python is strongly typed 
+# output--:                             
+"""
+hello
+hello
+
+6
+hell 6
+hello world
+5 first string"""
